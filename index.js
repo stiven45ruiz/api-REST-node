fd43d53b818f80872361f81f9c7162ad232dc863
Stiven Ruiz <stiven45ruiz@gmail.com>
2022-05-05 11:10:14 -0500

    boom error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 4000;
@@ -11,6 +11,7 @@ app.use(express.json());
 routerApi(app);
 
 app.use(logErrors);
+app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
--- services/productSevice.js
+++ services/productSevice.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom')
 
 class ProductsServices{
 
@@ -43,13 +44,17 @@ class ProductsServices{
 
   async findOne(id){
     const name = this.getTotal();
-    return this.products.find(item => item.id === id)
+    const product = this.products.find(item => item.id === id)
+    if (index === -1) {
+      throw boom.notFound('Product Not Found')
+    }
+    return product;
   }
 
   async update(id, changes){
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1){
-      throw new Error('product not found');
+      throw boom.notFound('Product Not Found');
     }
     const product = this.products[index]
     this.products[index] = {
@@ -62,7 +67,7 @@ class ProductsServices{
   async delete(id){
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1){
-      throw new Error('product not exist');
+      throw boom.notFound('Product Not Found');
     }
     this.products.splice(index, 1);
     return { message: `El producto con id ${id} se elimino` }
