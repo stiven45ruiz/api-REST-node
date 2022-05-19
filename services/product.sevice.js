9a3f917c3c106cf57e9c42059483dcf784dd2b2f
stiven45ruiz <stiven45ruiz@gmail.com>
2022-05-18 20:21:07 -0500

    deployment heroku

---
+++ services/product.sevice.js
@@ -0,0 +1,80 @@
+const faker = require('faker');
+const boom = require('@hapi/boom')
+
+class ProductsServices{
+
+  constructor(){
+    this.products = [];
+    this.generate();
+  }
+  async generate(){
+    const limit = 100;
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean(),
+      });
+    }
+  }
+
+  async create(product_name, price, image){
+    if(product_name || price || image === undefined){
+      throw new Error('There is something missing');
+    }
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...product_name,
+      ...price,
+      ...image
+    }
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  find(id){
+    return new Promise((resolve)=> {
+      setTimeout(()=>{
+        resolve(this.products);
+      }, 5000);
+    });
+  }
+
+  async findOne(id){
+
+    const product = this.products.find(item => item.id === id)
+    if (!product) {
+      throw boom.notFound('Product Not Found')
+    }
+    if(product.isBlock){
+      throw boom.conflict('Product is block')
+    }
+    return product;
+  }
+
+  async update(id, changes){
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1){
+      throw boom.notFound('Product Not Found');
+    }
+    const product = this.products[index]
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+  }
+
+  async delete(id){
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1){
+      throw boom.notFound('Product Not Found');
+    }
+    this.products.splice(index, 1);
+    return { message: `El producto con id ${id} se elimino` }
+  }
+}
+
+module.exports = ProductsServices;
