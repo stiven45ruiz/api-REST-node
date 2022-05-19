6beb8ef72ad11e8a44b91765c74e24ed5febb526
stiven45ruiz <stiven45ruiz@gmail.com>
2022-05-18 20:14:25 -0500

    deployment heroku

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
+
 const whiteList = [
   'http://localhost:8080',
   'https://myapp.com'
 ];
 const options = {
   origin: (origin, callback)=>{
-    if (whiteList.includes(origin) || origin){
+    if (whiteList.includes(origin) || !origin){
       callback(null, true)
     }else{
       callback(new Error('no permitido'))
